ac51d86e719c0a0967bbaffaa15873fbcb1fd868
Diego Acuña <diegoacuna.dev@gmail.com>
2022-08-11 13:42:25 -0500

    Segundo comentario

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -2,6 +2,7 @@
 
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
+//Prueba ahora que tal
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
